e709dde23f28ad9c437d7b9d50be42521a90bbdf
SonamYangchen77 <07230064.sherubtse@rub.edu.bt>
2025-04-02 10:32:32 +0600

    Practical 2

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,8 +5,10 @@ const { isAuthenticated } = require('../config/authMiddleware');
 
 // Profile route (only accessible by logged-in users)
 router.get('/profile', isAuthenticated, (req, res) => {
-    res.render('profile', { user: req.session.user });
+    if (!req.session.user) {
+        return res.redirect("/auth/login"); // Redirect if not logged in
+    }
+    res.render("profile", { user: req.session.user });
 });
-
 
 module.exports = router;
